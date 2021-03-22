ebba27b8cf9a3c058e7126c19dc4394c11502c3b
JSJ <jsj@tigrison.com>
2021-03-22 14:51:14 +0900

    changed 3

--- sample/src/main/java/com/starflow/junklibrary/sample/SampleActivity.kt
+++ sample/src/main/java/com/starflow/junklibrary/sample/SampleActivity.kt
@@ -1,16 +1,15 @@
-package com.starflow.junklibrary
+package com.starflow.junklibrary.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.starflow.junklibrary.gps.GpsController
 
-class JunkUtilTest: AppCompatActivity() {
+class SampleActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val gps = GpsController.getInstance(this)
-                .setGpsCallCount(5)
-                .start()
+        GpsController.getInstance(this)
+            .start()
     }
 }
